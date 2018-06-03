efe565bded4456b54bd249e6d0c113c134003aa4
Arun Asokan (admin) <paasokan@savethechildren.org.uk>
2018-06-03 18:54:40 +0100

    Add project files.

---
+++ FormsApplication/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace FormsApplication.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ FormsApplication/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FormsApplication.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
